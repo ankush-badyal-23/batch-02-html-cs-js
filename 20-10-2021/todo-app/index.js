60f1aab6eb3f3ef2bc700a25e051696303f27249
ankush-badyal-23 <86702078+ankush-badyal-23@users.noreply.github.com>
2021-10-21 16:07:44 +0530

    Todo app finished

--- 20-10-2021/todo-app/index.js
+++ 20-10-2021/todo-app/index.js
@@ -4,6 +4,7 @@ const list = document.querySelector("#task-list");
 
 window.onload = function () {
   const data = getList();
+  bindClickEvent();
   if (data) {
     list.innerHTML = data;
   }
@@ -15,12 +16,29 @@ addBtn.addEventListener("click", function () {
     return;
   }
   const item = document.createElement("li");
-  item.innerText = value;
-  list.appendChild(item);
+  const taskSpan = document.createElement('span');
+  const delSpan = document.createElement('span');
+  delSpan.classList.add('del-task');
+  delSpan.innerHTML = '&#9760;'
+  taskSpan.classList.add('task');
+  taskSpan.innerText = value;
+  item.append(taskSpan, delSpan);
+  list.append(item);
   saveList(list.innerHTML);
   input.value = "";
   input.focus();
 });
+
+function bindClickEvent() {
+  list.addEventListener("click", function(e) {
+    if(e.target.classList.contains('task')) {
+      e.target.classList.toggle('checked');
+    } else if(e.target.classList.contains('del-task')) {
+      e.target.parentElement.remove();
+    }
+    saveList(list.innerHTML);
+  });
+}
 
 function saveList(list) {
   localStorage.setItem("todo-list", list);
